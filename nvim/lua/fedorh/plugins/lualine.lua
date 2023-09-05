6d8b0e4177cf564d97fb30fa964e8d4281052031
Fedor Hajdu <fedor.hajdu@rebellion.co.uk>
2023-09-05 22:15:42 +0100

    lualine color settings

--- nvim/lua/fedorh/plugins/lualine.lua
+++ nvim/lua/fedorh/plugins/lualine.lua
@@ -4,4 +4,31 @@ if not status then
     return
 end
 
-lualine.setup()
+local theme = require("lualine.themes.nightfly")
+
+local new_colors = {
+    blue = "#65D1FF",
+    green = "#3EFFDC",
+    violet = "#FF61EF",
+    yellow = "#FFDA7B",
+    black = "#000000"
+}
+
+theme.normal.a.bg = new_colors.blue
+theme.insert.a.bg = new_colors.green
+theme.visual.a.bg = new_colors.violet
+theme.command = {
+    a = {
+        gui = "bold",
+        bg = new_colors.yellow,
+        fg = new_colors.black,
+    },
+}
+
+
+
+lualine.setup({
+    options = {
+        theme = theme
+    }
+})
