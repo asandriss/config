6d743edb9a8e64e0065ec248f2e58258c72f413c
Fedor Hajdu <fedor.hajdu@rebellion.co.uk>
2023-09-05 21:11:58 +0100

    fix packer auto compilation

--- nvim/lua/fedorh/plugins-setup.lua
+++ nvim/lua/fedorh/plugins-setup.lua
@@ -1,32 +1,39 @@
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- auto install packer if not installed
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
     return true
   end
   return false
 end
-local packer_bootstrap = ensure_packer()
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
--- reload neovim when this file is changed
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerCompile
-  augroup end
-]])
-
+-- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+  return
 end
+
+
 
 return packer.startup(function(use)
     use("wbthomason/packer.nvim")   -- packer plugin, it manages itself
 
     use("bluz71/vim-nightfly-guicolors") -- color scheme, it can be any repo
+
+    use("christoomey/vim-tmux-navigator")
 
     if packer_bootstrap then
         require("packer").sync()
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -83,6 +83,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/fedorh/.local/share/nvim/site/pack/packer/start/vim-nightfly-guicolors",
     url = "https://github.com/bluz71/vim-nightfly-guicolors"
+  },
+  ["vim-tmux-navigator"] = {
+    loaded = true,
+    path = "/home/fedorh/.local/share/nvim/site/pack/packer/start/vim-tmux-navigator",
+    url = "https://github.com/christoomey/vim-tmux-navigator"
   }
 }
 
